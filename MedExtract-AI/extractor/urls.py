66cae02eface0b2a103b1d4e7212d0cba702e8ac
Adrian Kaua <adriankaua0233@gmail.com>
2026-09-15 09:33:01 -0300

    remove duplicação da estrutura do projeto

--- MedExtract-AI/extractor/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.dashboard, name='dashboard'),
-    path('historico/apagar/', views.clear_history, name='clear_history'),
-]
